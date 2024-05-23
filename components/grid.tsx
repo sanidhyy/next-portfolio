5704649c2dd3ce64afbc6a25173c682f10d03c4c
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-05-23 14:33:44 +0000

    grid item updated

--- components/grid.tsx
+++ components/grid.tsx
@@ -5,30 +5,9 @@ export const Grid = () => {
   return (
     <section id="about">
       <BentoGrid>
-        {gridItems.map(
-          ({
-            id,
-            title,
-            description,
-            className,
-            img,
-            imgClassName,
-            titleClassName,
-            spareImg,
-          }) => (
-            <BentoGridItem
-              id={id}
-              key={id}
-              title={title}
-              description={description}
-              className={className}
-              img={img}
-              imgClassName={imgClassName}
-              titleClassName={titleClassName}
-              spareImg={spareImg}
-            />
-          )
-        )}
+        {gridItems.map((gridItem) => (
+          <BentoGridItem key={gridItem.id} {...gridItem} />
+        ))}
       </BentoGrid>
     </section>
   );
