5a4277aef2a0f6e5354ad86d9e643b6ec8d4d87f
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-05-23 08:02:38 +0000

    tech stack added

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -43,7 +43,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 relative group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none border border-white/[0.1] overflow-hidden relative justify-between flex flex-col space-y-4",
+        "row-span-1 relative group/bento rounded-3xl hover:shadow-xl transition duration-200 shadow-input dark:shadow-none border border-white/[0.1] overflow-hidden relative justify-between flex flex-col space-y-4",
         className
       )}
       style={{
@@ -96,9 +96,38 @@ export const BentoGridItem = ({
           <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
             {title}
           </div>
-        </div>
 
-        {id === 2 && <GridGlobe />}
+          {id === 2 && <GridGlobe />}
+
+          {id === 3 && (
+            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+              <div className="flex flex-col gap-3 lg:gap-8">
+                {["React.js", "Next.js", "Typescript"].map((item) => (
+                  <span
+                    key={item}
+                    className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132e]"
+                  >
+                    {item}
+                  </span>
+                ))}
+
+                <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]" />
+              </div>
+
+              <div className="flex flex-col gap-3 lg:gap-8">
+                <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]" />
+                {["Vue.js", "AWS", "MongoDB"].map((item) => (
+                  <span
+                    key={item}
+                    className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132e]"
+                  >
+                    {item}
+                  </span>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
