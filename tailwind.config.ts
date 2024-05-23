89cdc5fc826118bbb314c29c960d8c1df73a3a7b
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-05-23 06:44:19 +0000

    tailwind config updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        black: {
+          DEFAULT: "#000",
+          100: "#000319",
+        },
       },
     },
   },
