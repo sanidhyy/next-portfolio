407e7a70c6a3a807ffee196ee3719e6b12bbe98e
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-05-23 06:47:09 +0000

    spotlight component created

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: "0",
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+      },
       colors: {
         black: {
           DEFAULT: "#000",
