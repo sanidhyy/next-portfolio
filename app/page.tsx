806492e2eb0ad2185b9305687326506fab1f0743
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-05-23 07:10:34 +0000

    created floating nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
 import { Hero } from "@/components/hero";
+import { FloatingNav } from "@/components/ui/floating-nav";
+import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav
+          navItems={[
+            {
+              name: "Home",
+              link: "/",
+              icon: <FaHome />,
+            },
+          ]}
+        />
         <Hero />
       </div>
     </main>
