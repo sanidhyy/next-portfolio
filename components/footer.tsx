4b5050bc4987d8c1bac2eff498f72088c4795d4f
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-05-23 13:57:33 +0000

    footer links updated

--- components/footer.tsx
+++ components/footer.tsx
@@ -41,9 +41,12 @@ export const Footer = () => {
 
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((profile) => (
-            <div
+            <Link
               key={profile.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+              href={profile.link}
+              target="_blank"
+              rel="noreferrer noopener"
+              className="size-10 flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
               <img
                 src={profile.img}
@@ -51,7 +54,7 @@ export const Footer = () => {
                 width={20}
                 height={20}
               />
-            </div>
+            </Link>
           ))}
         </div>
       </div>
--- data/index.ts
+++ data/index.ts
@@ -205,13 +205,16 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
+    link: "https://github.com/sanidhyy",
   },
   {
     id: 2,
     img: "/twit.svg",
+    link: "https://twitter.com/TechnicalShubam",
   },
   {
     id: 3,
     img: "/link.svg",
+    link: "https://www.linkedin.com/in/sanidhyy",
   },
 ];
