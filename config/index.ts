727b9bfb57eb5cfddb24d5c3d36b420dd46dd1f3
Sanidhya Kumar Verma <sanidhyyy@gmail.com>
2026-08-21 19:22:29 +0530

    chore: update site url and author contact

--- config/index.ts
+++ config/index.ts
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 export const links = {
   sourceCode: "https://github.com/sanidhyy/next-portfolio",
   ownerName: "Sanidhya",
-  ownerEmail: "sanidhya.verma12345@gmail.com",
+  ownerEmail: "sanidhyyy@gmail.com",
 } as const;
 
 export const siteConfig: Metadata = {
